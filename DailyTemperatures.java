f759493c6797f11c59969d72b0bd8e1bd610100f
Dikshita Jain <dikshitajain2006@gmail.com>
2026-04-11 17:26:00 +0530

    Added Daily Temperatures solution using Stack

---
+++ DailyTemperatures.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();
+        int n = temperatures.length;
+        int[] res = new int[n];
+        for(int i=0;i<temperatures.length;i++){
+            while(!st.isEmpty() && temperatures[i]>temperatures[st.peek()]){
+                int idx = st.pop();
+                res[idx] = i-idx;
+            }
+            st.push(i);
+        }
+        return res;
+    }
+}
